54a1f7618891b96df508a9ecc4f97c8cfa4b39e2
wuseyu <96163054+wuseyu@users.noreply.github.com>
2024-11-26 20:01:19 +0800

    refactor: add type for function (#10)

--- predict/data_processing/process_data.py
+++ predict/data_processing/process_data.py
@@ -1,6 +1,6 @@
 from .load_data import load_csv_data, load_xls_data, load_xlsx_data, load_nc_data
 
-def load_data(file_path):
+def load_data(file_path: str):
     """Load data from file and return as xarray Dataset or pandas DataFrame."""
     if file_path.endswith('.nc'):
         return load_nc_data(file_path)
